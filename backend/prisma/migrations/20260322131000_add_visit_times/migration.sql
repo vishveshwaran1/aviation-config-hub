b83a4b1beaa394e05924c5548790d8fe505362c0
DharShan07vk <dharshanvk07@gmail.com>
2026-03-23 23:22:14 +0530

    Prisma error solved and reapplied

--- backend/prisma/migrations/20260322131000_add_visit_times/migration.sql
+++ backend/prisma/migrations/20260322131000_add_visit_times/migration.sql
@@ -1,4 +1,6 @@
-﻿-- AlterTable
-ALTER TABLE "AircraftComponent" ADD COLUMN     "cycle_since_visit" DOUBLE PRECISION,
-ADD COLUMN     "time_since_visit" DOUBLE PRECISION;
+-- AlterTable
+ALTER TABLE "AircraftComponent" ADD COLUMN "cycle_since_visit" DOUBLE PRECISION;
+ALTER TABLE "AircraftComponent" ADD COLUMN "time_since_visit" DOUBLE PRECISION;
 
+-- AlterTable
+ALTER TABLE "JourneyLog" ADD COLUMN "due_at_cycles" DOUBLE PRECISION;
