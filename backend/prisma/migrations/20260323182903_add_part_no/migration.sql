e1eff7d988b74bc295d8369253187256a3fd6a5c
DharShan07vk <dharshanvk07@gmail.com>
2026-03-24 00:29:58 +0530

    mirgrations

---
+++ backend/prisma/migrations/20260323182903_add_part_no/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Service" ADD COLUMN     "part_no" TEXT;
---
+++ backend/prisma/migrations/20260323185147_add_repeat_interval_unit/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Service" ADD COLUMN     "repeat_interval_unit" TEXT NOT NULL DEFAULT 'Hours';
